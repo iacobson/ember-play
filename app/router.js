1587b4279a73bdc3deffb3913ac92e9717ba2c5f
iacobson <dorian.iacobescu@gmail.com>
2016-04-03 21:07:16 +0100

    add Articles resource and nested route

--- app/router.js
+++ app/router.js
@@ -11,13 +11,22 @@ Router.map(function() {
 
     this.route('show', {
       path: ':friend_id'
+    }, function() {
+      // manually nesting articles under show
+      // `resetNamespace` will ensure a correct route:
+        // friends/1/articles
+        // instead of
+        // friends/show/articles
+      // also will allow us to define articles routes at top level, not inside friends directory
+      this.route('articles', {resetNamespace: true}, function() {
+
+      });
     });
 
     this.route('edit', {
       path: ':friend_id/edit'
     });
   });
-  this.route('articles');
 });
 
 export default Router;
