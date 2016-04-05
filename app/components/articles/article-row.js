dac4c6574a7ba7fe31b752ca90c338f446610f87
iacobson <dorian.iacobescu@gmail.com>
2016-04-05 08:37:20 +0100

    add article-row component

---
+++ app/components/articles/article-row.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'tr', // the html tag for the component (in this case a table row). If not specified, will be `div` by default
+  article: null, // passed-in
+  articleStates: null, //passed-in
+  // UNLIKE controllers and routes, the actions in COMPONENTS won't bubble up automatically
+  actions: {
+    saveArticle(article) {
+      // bind the action to a property and call it in the component with `sendAction`
+      // in this case we call the action save from the articles new route
+      this.sendAction('save', article)
+    }
+  }
+});
